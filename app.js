23df302b61ef9a7bfde8c6b789dc18e3be4a1166
Angela J. Holden <angelajholden@gmail.com>
2021-01-25 20:10:43 -0600

    more dynamic

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const video = document.getElementById("video");
+
 if (video) {
 	const tag = document.createElement("script");
-	tag.src = "//www.youtube.com/player_api";
+	tag.src = "https://www.youtube.com/player_api";
 	const firstScriptTag = document.getElementsByTagName("script")[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 }
 
+const title = "CMS unique module text title";
+const url = "https://www.youtube.com/watch?v=kZu3AYNjmjI";
+const urlId = url.split("=")[1];
+// console.log(url.split("=")[1]);
+
 let player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player("video", {
+		title: title,
+		videoId: urlId,
 		events: {
 			onReady: iframeModal,
 		},
@@ -19,6 +27,16 @@ function iframeModal() {
 	const container = document.querySelector(".embed-container");
 	const thumb = document.querySelector(".video_thumb");
 	const close = document.querySelector(".lines");
+
+	// const title = player.getVideoData().title;
+	// console.log(player.getVideoData());
+	// console.log(title);
+
+	const hqdefault = `<img src="https://img.youtube.com/vi/${urlId}/hqdefault.jpg" alt="${title}" />`;
+
+	if (thumb) {
+		thumb.insertAdjacentHTML("afterbegin", hqdefault);
+	}
 
 	thumb.addEventListener("click", () => {
 		const clicked = container.classList.contains("clicked");
@@ -44,5 +62,6 @@ function iframeModal() {
 }
 
 window.onload = () => {
-	iframeModal();
+	// onYouTubeIframeAPIReady();
+	// iframeModal();
 };
