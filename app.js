f6b85801525e4121b6efc4d2b1d99c7713305f90
Angela J. Holden <angelajholden@gmail.com>
2021-01-25 20:23:01 -0600

    make it all conditional

--- app.js
+++ app.js
@@ -5,60 +5,60 @@ if (video) {
 	tag.src = "https://www.youtube.com/player_api";
 	const firstScriptTag = document.getElementsByTagName("script")[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
 
-const title = "CMS unique module text title";
-const url = "https://www.youtube.com/watch?v=kZu3AYNjmjI";
-const urlId = url.split("=")[1];
-// console.log(url.split("=")[1]);
+	const title = "CMS unique module text title";
+	const url = "https://www.youtube.com/watch?v=kZu3AYNjmjI";
+	const urlId = url.split("=")[1];
+	// console.log(url.split("=")[1]);
 
-let player;
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player("video", {
-		title: title,
-		videoId: urlId,
-		events: {
-			onReady: iframeModal,
-		},
-	});
-}
-
-function iframeModal() {
-	const container = document.querySelector(".embed-container");
-	const thumb = document.querySelector(".video_thumb");
-	const close = document.querySelector(".lines");
-
-	// const title = player.getVideoData().title;
-	// console.log(player.getVideoData());
-	// console.log(title);
-
-	const hqdefault = `<img src="https://img.youtube.com/vi/${urlId}/hqdefault.jpg" alt="${title}" />`;
-
-	if (thumb) {
-		thumb.insertAdjacentHTML("afterbegin", hqdefault);
+	let player;
+	function onYouTubeIframeAPIReady() {
+		player = new YT.Player("video", {
+			title: title,
+			videoId: urlId,
+			events: {
+				onReady: iframeModal,
+			},
+		});
 	}
 
-	thumb.addEventListener("click", () => {
-		const clicked = container.classList.contains("clicked");
+	function iframeModal() {
+		const container = document.querySelector(".embed-container");
+		const thumb = document.querySelector(".video_thumb");
+		const close = document.querySelector(".lines");
 
-		if (!clicked) {
-			container.classList.add("clicked");
-			thumb.classList.add("overlay");
-			close.classList.add("active");
-			player.playVideo();
+		// const title = player.getVideoData().title;
+		// console.log(player.getVideoData());
+		// console.log(title);
+
+		const hqdefault = `<img src="https://img.youtube.com/vi/${urlId}/hqdefault.jpg" alt="${title}" />`;
+
+		if (thumb) {
+			thumb.insertAdjacentHTML("afterbegin", hqdefault);
 		}
-	});
 
-	close.addEventListener("click", () => {
-		const active = close.classList.contains("active");
+		thumb.addEventListener("click", () => {
+			const clicked = container.classList.contains("clicked");
 
-		if (active) {
-			container.classList.remove("clicked");
-			thumb.classList.remove("overlay");
-			close.classList.remove("active");
-			player.pauseVideo();
-		}
-	});
+			if (!clicked) {
+				container.classList.add("clicked");
+				thumb.classList.add("overlay");
+				close.classList.add("active");
+				player.playVideo();
+			}
+		});
+
+		close.addEventListener("click", () => {
+			const active = close.classList.contains("active");
+
+			if (active) {
+				container.classList.remove("clicked");
+				thumb.classList.remove("overlay");
+				close.classList.remove("active");
+				player.pauseVideo();
+			}
+		});
+	}
 }
 
 window.onload = () => {
